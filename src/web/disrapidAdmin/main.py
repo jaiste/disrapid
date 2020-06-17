09c40017f3d4a3e8a082005c9de5c27b8d92b437
xcad2k <28359525+xcad2k@users.noreply.github.com>
2020-06-17 10:43:39 +0200

    basic webadmin update1

--- src/web/disrapidAdmin/main.py
+++ src/web/disrapidAdmin/main.py
@@ -3,21 +3,20 @@ from flask import (
     render_template,
     send_from_directory,
     redirect,
+    url_for,
     request,
     session,
     flash,
-    jsonify,
 )
 
 import logging
 import os
 import sys
-import re
 from functools import wraps
 from requests_oauthlib import OAuth2Session
 
 from db.interface import DisrapidDb
-from db.guild import Guild, Role, Channel, Welcomemessage
+# from db.guild import Guild, Role, Channel, Welcomemessage
 
 import ptvsd
 
@@ -29,14 +28,17 @@ try:
         # in debug mode we need to wait for debugger to connect
         ptvsd.wait_for_attach()
         logging.basicConfig(level=logging.DEBUG)
+        debug = True
     else:
         # debug mode is not enabled, running in production mode...
-        pass
+        debug = False
 
 except Exception:
     # any error will stop the container
     sys.exit(1)
 
+
+ADMINISTRATOR = 0x00000008
 
 app = Flask(__name__)
 
@@ -65,6 +67,10 @@ except Exception as e:
     sys.exit(1)
 
 
+def token_updater(token):
+    session['oauth2_token'] = token
+
+
 def make_session(token=None, state=None, scope=None):
     return OAuth2Session(
         client_id=OAUTH2_CLIENT_ID,
@@ -81,44 +87,30 @@ def make_session(token=None, state=None, scope=None):
         )
 
 
-def is_logged_in(f):
-    # this represents the decorator to check whether a user has a valid
-    # discord oauth token
-    # ---
-    #
+def logged_in(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if 'oauth2_token' in session:
-            # ADD IMPLEMENTATION:
-            # check if token is still valid!!
-            #
-            # TMP
             return f(*args, **kwargs)
         else:
-            # flash('Unauthorized, please login', 'danger')
             return redirect(url_for('view_login'))
     return wrap
 
 
-def is_server_selected(f):
-    # This is the decorator to check whether the user has selected a server
-    # ---
-    #
+def guild_selected(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if 'server_id' in session:
+        if 'guild_id' in session:
             return f(*args, **kwargs)
         else:
             flash('Select your server first!', 'info')
-            return redirect(url_for('view_select_server'))
+            return redirect(url_for('view_select_guild'))
     return wrap
 
 
 @app.route('/assets/<path:path>')
 def view_assets(path):
     # Assets are visible without login
-    # ---
-    #
     return send_from_directory('assets', path)
 
 
@@ -145,13 +137,13 @@ def view_login():
 
 
 @app.route('/logout')
-def logout(): 
+def logout():
     # Log the user out and redirect to login-page
     # ---
     #
     session.clear()
     flash('You are now logged out', 'success')
-    return redirect(url_for('view_login'))
+    return redirect(LOGOUT_REDIRECT_URL)
 
 
 @app.route('/callback')
@@ -165,14 +157,16 @@ def callback():
     try:
         if request.values.get('error'):
             return request.values['error']
+
         discord = make_session(state=session.get('oauth2_state'))
+
         token = discord.fetch_token(
             TOKEN_URL,
             client_secret=OAUTH2_CLIENT_SECRET,
             authorization_response=request.url.replace('http', 'https'))
+
         session['oauth2_token'] = token
         session['logged_in'] = True
-        logging.debug(f'user logged in with token: {token}')
 
         # get user data
         discord = make_session(token=session.get('oauth2_token'))
@@ -184,33 +178,45 @@ def callback():
         session['user_data'] = discord.get(API_BASE_URL + '/users/@me') \
             .json()
 
-        logging.debug(session['user_data'])
-        return redirect(url_for('view_select_server'))
+        return redirect(url_for('view_select_guild'))
     except Exception as e:
         logging.error(e)
 
+
+@app.route('/select_guild', methods=['GET'])
+@logged_in
+def view_select_guild():
+    pass
+
+
+@app.route('/select_guild', methods=['POST'])
+@logged_in
+def post_select_guild():
+    pass
+
+
 # @app.route('/select_server')
-# @is_logged_in
-# def view_select_server():
+# @logged_in
+# def view_select_guild():
 #     # This represents the server selector function
 #     # ---
-#     # 
-#     # Once the user is logged in we need him to select a server first
-#     # We collect all servers the user has access to and show them all servers he is admin compared with our xbot database
-#     # If the user has not invited xbot to this server, we will present him a link to do so
 #     #
-#     # Parameters
-#     # ---
-#     # server_id: :class: `string`
-#     #   user has selected server with this id
-#     # 
+#     # Once the user is logged in we need him to select a server first
+#     # We collect all servers the user has access to and show them all servers
+#     # he is admin compared with our disrapid database
+#     # If the user has not invited disrapid to this server, we will present him
+#     # a link to do so
 # 
-#     # get parameters
-#     server_id = request.args.get("server_id")
+#     dbsession = db.Session()
+# 
+#     guild_id = request.args.get("guild_id")
 #     ip_addr = request.remote_addr
 # 
-#     if not server_id:
-#         # user has not selected a server, show a list of all servers the user can select
+#     dbguilds = dbsession.query
+#     
+#     if not guild_id:
+#         # user has not selected a server, show a list of all servers the user
+#         # can select
 # 
 #         respond_servers = []
 # 
@@ -220,60 +226,57 @@ def callback():
 #             
 #             # permission calculation
 #             if guild['permissions'] & ADMINISTRATOR == ADMINISTRATOR:
-#                 # user is admin on this server, check if xbot has joined this server
-#                 newserver = xdb.get_server(guild['id'])
+#                 # user is admin on this server, check if disrapid has joined
+#                 # this server
+#                 
+#                 
+# 
 #                 if newserver != None:
-#                     # xbot has joined this server before
+#                     # disrapid has joined this server before
 #                     logging.debug(f"server: {newserver}")
 #                     newserver["guild_avatar"] = guild["icon"]
 #                     respond_servers.append(newserver)
 #                 else:
-#                     # xbot hasn't joined this server
+#                     # disrapid hasn't joined this server
 #                     #
 #                     # ADD IMPLEMENTATION
 #                     # --
-#                     # show this server as "new server", invite xbot to this server
+#                     # show this server as "new server", invite disrapid to
+#                     # this server
 #                     pass
 #         
 #         return render_template('select_server.html', servers=respond_servers)
 #     else:
 #         # user has selected server,
-#         # check if user has access to server_id
-#         if not only_number(server_id):
-#             logging.warning(f'[RISK] from ip_addr-{ip_addr} user-{session["user_data"]["id"]} ({session["user_data"]["username"]}) has tried to modify server_id input!')
+#         # check if user has access to guild_id
+#         if not only_number(guild_id):
+#             logging.warning(f'[RISK] from ip_addr-{ip_addr} user-{session["user_data"]["id"]} ({session["user_data"]["username"]}) has tried to modify guild_id input!')
 #             flash('Server_id violation!', 'danger')
 #             return redirect(url_for('view_select_server'))
 # 
 #         for guild in session['guild_data']:
-#             if server_id == guild['id']:
-#                 session['server_id'] = server_id
+#             if guild_id == guild['id']:
+#                 session['guild_id'] = guild_id
 #                 session['server_name'] = guild['name']
 #                 break
 #         
-#         if 'server_id' not in session:
+#         if 'guild_id' not in session:
 #             # user has no acces to this server!
-#             # this shouldn`t happen in normal environment, alert admin with a risk log
-#             logging.warning(f'[RISK] from ip_addr-{ip_addr} user-{session["user_data"]["id"]} ({session["user_data"]["username"]}) has tried to select server-{server_id} where he has no permissions!')
+#             # this shouldn`t happen in normal environment, alert admin with
+#             # a risk log
+#             logging.warning(f'[RISK] from ip_addr-{ip_addr} user-{session["user_data"]["id"]} ({session["user_data"]["username"]}) has tried to select server-{guild_id} where he has no permissions!')
 #             flash('You are not allowed to access this server!', 'danger')
 #             return redirect(url_for('view_select_server'))
 # 
 #         return redirect(url_for('view_dashboard'))
 
-# @app.route('/')
-# @is_logged_in
-# @is_server_selected
-# def view_dashboard():
-#     # get dashboard
-#     server_info = xdb.get_server(session["server_id"])
-#     # read server meta
-#     meta_info = xdb.get_server_meta(session["server_id"])
-#     return render_template('dashboard.html', meta=meta_info, server=server_info)
-
 
 @app.route('/')
+# @logged_in
+# @guild_selected
 def view_dashboard():
     return render_template('dashboard.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=debug)
