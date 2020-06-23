b812404eab0911fe600d0c2c842123c7bdb4d38f
xcad2k <28359525+xcad2k@users.noreply.github.com>
2020-06-23 18:10:30 +0200

    prepare wtfforms

--- src/web/disrapidAdmin/main.py
+++ src/web/disrapidAdmin/main.py
@@ -9,6 +9,9 @@ from flask import (
     flash,
     abort,
 )
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, TextField, SubmitField
+# from wtforms.validators import DataRequired, Length
 
 import logging
 import os
