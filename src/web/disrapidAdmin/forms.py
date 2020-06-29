1a5c2046b8438d98fc4496833a8a537a8ab91650
xcad2k <lempa.christian@gmail.com>
2020-06-29 21:57:43 +0200

    updates1

--- src/web/disrapidAdmin/forms.py
+++ src/web/disrapidAdmin/forms.py
@@ -1,16 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
+from wtforms import BooleanField, TextField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
 class WelcomeForm(FlaskForm):
-    # name = StringField('Name', [
-    #     DataRequired()])
-    # email = StringField('Email', [
-    #     Email(message=('Not a valid email address.')),
-    #     DataRequired()])
-    # body = TextField('Message', [
-    #     DataRequired(),
-    #     Length(min=4, message=('Your message is too short.'))])
-    # recaptcha = RecaptchaField()
-    # submit = SubmitField('Submit')
+    enabled = BooleanField('Enabled', [
+        DataRequired()])
+    message = TextField('Message', [
+        DataRequired(),
+        Length(max=2000, message=('Only 2000 characters allowed'))])
+    submit = SubmitField('Submit')
